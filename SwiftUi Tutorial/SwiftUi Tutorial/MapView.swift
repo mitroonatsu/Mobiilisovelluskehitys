e2595ff7417d93a60a8ecadfeaf9a84de796c130
Mitro Onatsu <mitro.onatsu@hotmail.com>
2020-01-13 09:41:40 +0200

    SwiftUi Tutorial started and done. Pushing to Github works

---
+++ SwiftUi Tutorial/SwiftUi Tutorial/MapView.swift
@@ -0,0 +1,28 @@
+//
+//  MapView.swift
+//  SwiftUi Tutorial
+//
+//  Created by Mitro Onatsu on 13/01/2020.
+//  Copyright © 2020 Mitro Onatsu. All rights reserved.
+//
+
+import SwiftUI
+import MapKit
+
+struct MapView: UIViewRepresentable {
+    func makeUIView(context: Context) -> MKMapView {
+            MKMapView(frame: .zero)
+        }
+    func updateUIView(_ view: MKMapView, context: Context) {
+        let coordinate = CLLocationCoordinate2D(
+            latitude: 34.011286, longitude: -116.166868)
+        let span = MKCoordinateSpan(latitudeDelta: 2.0, longitudeDelta: 2.0)
+        let region = MKCoordinateRegion(center: coordinate, span: span)
+        view.setRegion(region, animated: true)
+    }}
+
+struct MapView_Previews: PreviewProvider {
+    static var previews: some View {
+        MapView()
+    }
+}
