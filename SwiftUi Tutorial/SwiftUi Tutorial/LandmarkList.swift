0e10e6427c4f79ff62656b57eb784255b73fd7b2
Mitro Onatsu <mitro.onatsu@hotmail.com>
2020-01-27 08:23:16 +0200

    Added userdata.swift.  Started working on favourite marks

--- SwiftUi Tutorial/SwiftUi Tutorial/LandmarkList.swift
+++ SwiftUi Tutorial/SwiftUi Tutorial/LandmarkList.swift
@@ -9,18 +9,38 @@
 import SwiftUI
 
 struct LandmarkList: View {
+    @State private var landmark = landmarkData
+    @State private var showingAlert = false
     var body: some View {
         NavigationView {
-            List(landmarkData, id: \.id) { landmark in
+            List{
+                ForEach(landmark, id: \.id){ landmark in
             NavigationLink(destination: LandmarkDetail(landmark: landmark)) {
                 LandmarkRow(landmark: landmark)
                    }
                 }
+                .onDelete(perform: delete)
+                .onMove(perform: move)
+            }
             .navigationBarTitle(Text("Landmarks"))
+            .navigationBarItems(trailing: EditButton())
                 }
+        
+        .alert(isPresented: $showingAlert) {
+            Alert(title: Text("Do you want to delete?"), primaryButton: .destructive(Text("Confirm")), secondaryButton: .cancel(Text("Cancel")))
             }
         }
-
+    
+    
+    func delete(at offsets: IndexSet){
+        showingAlert = true
+        //landmark.remove(atOffsets: offsets)
+    }
+    
+    func move(from source: IndexSet, to destination: Int){
+        landmark.move(fromOffsets: source, toOffset: destination)
+    }
+}
 struct LandmarkList_Previews: PreviewProvider {
     static var previews: some View {
         ForEach(["iPhone SE", "iPhone XS Max"], id: \.self) { deviceName in
